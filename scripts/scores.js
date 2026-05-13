7862792c8a479a8f1777a2d21cc25b4ddeebf132
Ayala_Daitch <ayala33081@gmail.com>
2026-05-13 21:46:50 +0300

    Add scores.js to manage settings modal functionality

---
+++ scripts/scores.js
@@ -0,0 +1,6 @@
+import { setupModal } from './main.js';
+
+// =========================================
+// --- ניהול מודאל ההגדרות ---
+// =========================================
+setupModal('btn-open-settings', 'btn-close-settings', 'modal-settings');
