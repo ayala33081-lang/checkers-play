815f5ed337963fb040a63b7d6587e34943c8c05b
Ayala_Daitch <ayala33081@gmail.com>
2026-05-13 21:32:12 +0300

    Add instructions page logic to import shared settings and connect modal

---
+++ scripts/instructions.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview לוגיקת דף ההוראות - מייבא הגדרות משותפות ומחבר מודאל
+ * @module instructions
+ */
+
+import { setupModal } from './main.js';
+
+// --- חיבור מודאל ההגדרות ---
+setupModal('btn-open-settings', 'btn-close-settings', 'modal-settings');
